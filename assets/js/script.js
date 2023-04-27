5e0563c757940128c50d560cb9ed9d4db445fbd1
Noob Evad <davefrankmiranda@gmail.com>
2023-04-27 14:49:05 -0500

    added Yasmeen's carbonAPI stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
-/*
-carbonURL = 'https://www.carboninterface.com/api/v1/estimates';
-
-fetch(carbonURL, {
+fetch('https://www.carboninterface.com/api/v1/estimates', {
   method: 'POST',
   headers: {
     'Authorization': 'Bearer SBghLVQMlzBnpWbfkjr1Kg',
@@ -10,18 +7,16 @@ fetch(carbonURL, {
   body: JSON.stringify({
     type: 'vehicle',
     distance_unit: 'mi',
-    distance_value: 200,
+    distance_value: 100,
     vehicle_model_id: '7268a9b7-17e8-4c8d-acca-57059252afe9'
   })
 })
   .then(response => response.json())
   .then(data => {
     console.log(data);
-    const carbonLoad = data.attributes.vehicle_year;
-    console.log(carbonLoad);
+    console.log(data.attributes);
   })
   .catch(error => console.error(error))
-*/
 
 
 // bing API key
