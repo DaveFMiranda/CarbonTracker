a3038331c54007644e551ad9ef5d611e0790afb4
Yasmeen <127054048+Yasmeeeenn@users.noreply.github.com>
2023-04-27 12:25:56 -0500

    working on adding data/running

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 // ^^ this resulted in status code 404 "not found" logged.
 var carbonAPIKey = "SBghLVQMlzBnpWbfkjr1Kg";
 
-//estimate for vehicles 
-var carbonBaseURL= "https://www.carboninterface.com/api/v1/estimates";
-
+function fetchData(){
+ //estimate for vehicles 
+var carbonBaseURL= "https://www.carboninterface.com/api/v1/estimates";   
 
 // Set up the request headers
 var headers = {
@@ -12,9 +12,27 @@ var headers = {
   'Content-Type': 'application/json'
 };
 
+var requestData = {
+    // Include the data you want to send in the request
+    "distance_value": 100.0,
+      "vehicle_make": "toyota",
+      "vehicle_model": "corolla",
+      "vehicle_year": 2017,
+      "distance_unit": "mi"
+  };
+
+//convert the request data to query parameters
+  //var queryParams = new URLSearchParams(requestData);
+
+//append the query parameters to the URL
+//var urlWithParams = '${carbonBaseURL}?${queryParams}';
+
 // Make the GET request to the API
-fetch(carbonBaseURL, { headers })
-  .then(response => {
+fetch(carbonBaseURL, {
+    method: 'POST', headers, body: JSON.stringify(requestData)
+ })
+
+    .then(response => {
     if (response.ok) {
       return response.json();
     } else {
@@ -28,3 +46,6 @@ fetch(carbonBaseURL, { headers })
   .catch(error => {
     console.error(error);
   });
+};
+  //call the function to test
+  fetchData();
