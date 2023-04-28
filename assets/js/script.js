c82d26273412e5ebd17bdd091318e451811f1cf8
Noob Evad <davefrankmiranda@gmail.com>
2023-04-28 14:48:34 -0500

    some slight text tweaks and visual code cleanup by Dave, accidentally made in the dev branch sorry

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,26 +29,21 @@ service.getDistanceMatrix(
 
 // Return data from DMS
 function callback(response, status) {
-
   statusCheck = response.rows[0].elements[0].status;
-  console.log(statusCheck);
   if (statusCheck == 'NOT_FOUND') {
     alert("Distance information is not available. If there is more than one place named " + origin2.value + " or " + destinationA.value + ", please be more specific.");
   }
   if (statusCheck == "ZERO_RESULTS"){
-    alert("You can't get there form here.");
+    alert("You can't get there from here.");
   }
       if (status == 'OK') {
           var origins = response.originAddresses;
           var destinations = response.destinationAddresses;
-      
-          //These for loops are for multi-stop trips
+          //These for loops would be for multi-stop trips
           for (var i = 0; i < origins.length; i++) {
             var results = response.rows[i].elements;
             for (var j = 0; j < results.length; j++) {
               var element = results[j];
-             
-          
               distanceWord = element.distance.text;
               // Removes any commas in the return data so the carbontracker doesn't get confused and turn commas into decimal points
               distanceNumber = distanceWord.replace(/,/g, "");
@@ -58,9 +53,7 @@ function callback(response, status) {
               var to = destinations[j];
             }
           }
-        } 
-        console.log(response);
-          
+        }           
         console.log(distance);
 
         // Launches the carbontracker, sends it the distance received above
@@ -81,12 +74,10 @@ function callback(response, status) {
           })
             .then(response => response.json())
             .then(data => {
-              console.log(data);
-              console.log(data.data.attributes.carbon_lb);
               carbonOutput = data.data.attributes.carbon_lb;
               // Creates an element to display the result and displays the result
               var output = document.createElement('output');
-              output.textContent = 'Pounds of carbon created: ' + carbonOutput;
+              output.textContent = 'Driving from ' + origins + ' to ' + destinations + ' will release ' + carbonOutput + ' pounds of carbon into the atmosphere.';
               document.body.appendChild(output);
               // Stores the carbon output to local storage
               localStorage.setItem(localStorage.length+1, carbonOutput);
@@ -97,26 +88,18 @@ function callback(response, status) {
               for (var k = 0; k < localStorage.length; k++) {
                 totalCarbonArray.push(parseInt(localStorage.getItem(k+1)));
               }
-              console.log(totalCarbonArray);
               // Puts out the sum of the numbers in the array/local storage
               for (var m = 0; m < totalCarbonArray.length; m++){
                   totalCarbonNumber += totalCarbonArray[m];
-                  console.log(totalCarbonNumber);
                 }
-              
-              // create a field to display total carbon burned (totalCarbonNumber) [this should be hard-coded into the html] Is there a way to have that number load upon page load?
-                // html hardcode a clear button that clears local storage with text "Reset total carbon burned" // Clear button onclick.(localStorage.clear())
+                console.log(totalCarbonNumber);
+              // Displays the total carbon output
               var outputTotal = document.createElement('outputTotal');
-              outputTotal.textContent = 'Total pounds of carbon tracked: ' + totalCarbonNumber;
+              outputTotal.textContent = 'Driving all of the trips you\'ve searched so far would release ' + totalCarbonNumber + ' pounds of carbon into the atmosphere.';
               document.body.appendChild(outputTotal);
-
-    
-
-        
             })
             .catch(error => console.error(error))
           }
           carbonAPI();
       }       
-
     }
