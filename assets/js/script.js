dfcfd606396063e995b56f2061c1ff79b7fb2ec2
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-04-27 21:21:27 -0500

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,35 +49,52 @@ function callback(response, status) {
           }
         }
         console.log(distance);
-
-        // Launches the carbontracker, sends it the distance recieved above
-        function carbonAPI() {
-          fetch('https://www.carboninterface.com/api/v1/estimates', {
-            method: 'POST',
-            headers: {
-              'Authorization': 'Bearer Qx7s1muNYFpoAmHwkVH88Q',
-              'Content-Type': 'application/json'
-            },
-            // This could be updated to use different types of transport, unit names, and makes/models of car
-            body: JSON.stringify({
-              type: 'vehicle',
-              distance_unit: 'mi',              
-              distance_value: distance,
-              vehicle_model_id: '7268a9b7-17e8-4c8d-acca-57059252afe9'
-            })
-          })
-            .then(response => response.json())
-            .then(data => {
-              console.log(data);
-              console.log(data.data.attributes.carbon_lb);
-              carbonOutput = data.data.attributes.carbon_lb;
-              // Creates an element to display the result and displays the result
-              var output = document.createElement('output');
-              output.textContent = 'Pounds of carbon created: ' + carbonOutput;
-              document.body.appendChild(output);
-            })
-            .catch(error => console.error(error))
-          }
-          carbonAPI();
-      }
     }
+    function callback(response, status) {
+        if (status == 'OK') {
+          var origins = response.originAddresses;
+          var destinations = response.destinationAddresses;
+      
+          //These for loops are for multi-stop trips
+          for (var i = 0; i < origins.length; i++) {
+            var results = response.rows[i].elements;
+            for (var j = 0; j < results.length; j++) {
+              var element = results[j];
+              if (element.distance && element.distance.text) {
+                // distance text exists
+              } else {
+                alert("Distance information is not available");
+              }
+               
+              distanceWord = element.distance.text;
+              // Removes any commas in the return data so the carbontracker doesn't get confused and turn commas into decimal points
+              distanceNumber = distanceWord.replace(/,/g, "");
+              var distance = distanceNumber;
+              // var duration = element.duration.text;
+              var from = origins[i];
+              var to = destinations[j];
+              // Display an alert message with the calculated distance
+                alert("The distance between " + from + " and " + to + " is " + distance + " miles.");
+
+            }
+          }
+          console.log(distance);
+        } else {
+          // Handle error status codes
+          if (status == 'INVALID_REQUEST') {
+            alert("The request was invalid. Please try again.");
+          } else if (status == 'MAX_ELEMENTS_EXCEEDED') {
+            alert("The product of origins and destinations exceeds the per-query limit. Please try a smaller area.");
+          } else if (status == 'OVER_QUERY_LIMIT') {
+            alert("The application has exceeded its quota limit. Please try again later.");
+          } else if (status == 'REQUEST_DENIED') {
+            alert("The request was denied. Please check your API key.");
+          } else if (status == 'UNKNOWN_ERROR') {
+            alert("An unknown error occurred. Please try again.");
+          } else if (status == 'ZERO_RESULTS') {
+            alert("No results found for the requested locations.");
+          }
+        }
+    };
+  }
+  
