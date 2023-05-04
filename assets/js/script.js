49a0184f203f359b2becd9ce2f312b44f1dfce59
Noob Evad <davefrankmiranda@gmail.com>
2023-05-04 13:18:06 -0500

    minor text and color tweaks, formatted

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,31 +91,14 @@ function calculateCarbon() {
             origins +
             " to " +
             destinations +
-            " will be " + distance + ", release " +
+            " will be " +
+            distance +
+            ", release " +
             carbonOutput +
-            " pounds of carbon into the atmosphere, and take " + duration + '.';
+            " pounds of carbon into the atmosphere, and take " +
+            duration +
+            ".";
           document.body.appendChild(output);
-
-          // THIS IS WHERE THE LOCAL STORAGE AND RETRIEVAL USED TO BE BUT WE PROBABLY DON'T NEED IT ANYMORE
-          /*
-              totalCarbonNumber = 0;
-              // Creates an array to receive data from local storage
-              totalCarbonArray = [];
-              // Converts each local storage value to a number and pushes it to the above array
-              for (var k = 0; k < localStorage.length; k++) {
-                totalCarbonArray.push(parseInt(localStorage.getItem(k+1)));
-              }
-              // Puts out the sum of the numbers in the array/local storage
-              for (var m = 0; m < totalCarbonArray.length; m++){
-                  totalCarbonNumber += totalCarbonArray[m];
-                }
-                console.log(totalCarbonNumber);
-              // Displays the total carbon output
-              var outputTotal = document.createElement('div');
-              outputTotal.setAttribute('id', 'outputTotal');
-              outputTotal.textContent = 'Driving all of the trips you\'ve searched so far would release ' + totalCarbonNumber + ' pounds of carbon into the atmosphere.';
-              document.body.appendChild(outputTotal);
-              */
         })
         .catch((error) => console.error(error));
     }
@@ -123,117 +106,9 @@ function calculateCarbon() {
   }
 }
 
-// THIS WAS THE OLD RESET BUTTON AND CAN BE DELETED
-/*
-var reset = document.getElementById('reset');
-reset.addEventListener('click', function(resetButton) {
-  var clearOutput = document.getElementById('output');
-  clearOutput.remove();
-  var clearOutputTotal = document.getElementById('outputTotal');
-  clearOutputTotal.remove();
-})
-
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 100,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-      }}
-    }})
-
-*/
-
 // Stores the carbon output to local storage
 function saveToLocalStorage() {
   localStorage.setItem(localStorage.length + 1, carbonOutput);
-  // displayAlways();
   clearDisplay();
 }
 
@@ -241,12 +116,10 @@ function saveToLocalStorage() {
 function clearDisplay() {
   var clearOutput = document.getElementById("output");
   clearOutput.remove();
-  // var clearOutputTotal = document.getElementById('outputTotal');
-  // clearOutputTotal.remove();
 }
 function clearStorage() {
-  var totalResults = document.getElementById('totalResults');
-  totalResults.innerHTML = '0 lbs of carbon';
+  var totalResults = document.getElementById("totalResults");
+  totalResults.innerHTML = "0 lbs of carbon";
 }
 //function for sum of total stored
 function calculatedTotal() {
@@ -268,17 +141,10 @@ function calculatedTotal() {
 //displays total sum on screen load
 function displayAlways() {
   calculatedTotal();
-  /*
-  var displayArray = document.createElement("p")
-  displayArray.textContent = "Trip distances Saved: " + totalCarbonArray;
-*/
   var displayCarbon = document.createElement("p");
   displayCarbon.textContent = totalCarbonNumber + " lbs of carbon";
-
-  // var displayTable = document.getElementById("totalResults");
-  // displayTable.appendChild(displayArray);
-  var totalResults = document.getElementById('totalResults');
-  totalResults.innerHTML = '';
+  var totalResults = document.getElementById("totalResults");
+  totalResults.innerHTML = "";
   totalResults.appendChild(displayCarbon);
 }
 displayAlways();
@@ -379,4 +245,3 @@ particlesJS("particles-js", {
     },
   },
 });
-
